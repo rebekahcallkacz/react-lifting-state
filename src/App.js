f63bf8f0836be2cf2836a802c884df94f40fa535
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2022-11-17 18:31:04 -0500

    plan for demo

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import Counter from "./Counter";
 import CounterDisplay from "./CounterDisplay";
 
 function App() {
+  // when click count is changed, store that new value in local storage
+  // on initial render, set click count as value from local storage
+  // create custom hook that does both these things for us
+
   const [clickCount, setClickCount] = useState(0);
-  console.log("clickCount:", clickCount);
+  // console.log("clickCount:", clickCount);
   return (
     <div>
       <Counter clickCount={clickCount} setClickCount={setClickCount} />
