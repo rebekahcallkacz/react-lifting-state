a845f161a5c61e02f250f7adc7010fa6cd4d9660
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2022-11-17 20:04:49 -0500

    add a useEffect block and a custom hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 
 import Counter from "./Counter";
 import CounterDisplay from "./CounterDisplay";
 
-function App() {
-  // when click count is changed, store that new value in local storage
-  // on initial render, set click count as value from local storage
-  // create custom hook that does both these things for us
-
+// I create custom hooks when I want to be able to reuse them for multiple components
+// It isn't necessary here, but I'm including it for the sake of the demo
+const useCustomState = () => {
   const [clickCount, setClickCount] = useState(0);
-  // console.log("clickCount:", clickCount);
+
+  // This will only run when the variable clickCount changes
+  useEffect(() => {
+    console.log("the click count changed!");
+    console.log(clickCount);
+  }, [clickCount]);
+
+  return [clickCount, setClickCount];
+};
+
+function App() {
+  const [clickCount, setClickCount] = useCustomState();
+
   return (
     <div>
-      <Counter clickCount={clickCount} setClickCount={setClickCount} />
+      <Counter
+        clickCount={clickCount}
+        setClickCount={setClickCount}
+        label={"Click Me 1"}
+      />
+      <Counter
+        clickCount={clickCount}
+        setClickCount={setClickCount}
+        label={"Click Me 2"}
+      />
       <CounterDisplay clickCount={clickCount} />
     </div>
   );
