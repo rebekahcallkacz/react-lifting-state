5aaaf6de0866672f9446b995c7c9c0c531dfac72
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2023-04-11 20:05:07 -0400

    add two buttons managed separately

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import { useState } from "react";
 
+import CountDisplay from "./CountDisplay";
 import Counter from "./Counter";
 
 function App() {
+  const [buttonOneClicks, setButtonOneClicks] = useState(0);
+  const [buttonTwoClicks, setButtonTwoClicks] = useState(0);
+
   return (
     <div>
-      <Counter style={{ margin: "auto" }} />
+      <Counter clicks={buttonOneClicks} setClicks={setButtonOneClicks} label={"Button 1"} style={{ margin: "auto" }} />
+      <Counter clicks={buttonTwoClicks} setClicks={setButtonTwoClicks} label={"Button 2"} style={{ margin: "auto" }} />
+      <CountDisplay clicks={buttonOneClicks} buttonLabel={"1"}/>
+      <CountDisplay clicks={buttonTwoClicks} buttonLabel={"2"}/>
     </div>
   );
 }
